094900976e305d93bf1834e9e0acefc054509ee7
Liliya <lilyashushka@gmail.com>
2026-02-12 21:52:56 +0200

    [FIX Media] and add burger

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,16 @@
   };
 }
 ());
+//Burger start//
+(function (){
+const burgerItem = document.querySelector('.burger');
+const menu = document.querySelector('.header_nav');
+const menuCloseItem = document.querySelector('.header_nav-close');
+burgerItem.addEventListener('click', () => {
+menu.classList.add('.header_nav_active');
+});
+menuCloseItem.addEventListener('click', () => {
+  menu.classList.remove('.header_nav_active');
+});
+}());
+//Burger end//
