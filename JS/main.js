79799c2248739b79e160a338062c2aba1e577405
Liliya <lilyashushka@gmail.com>
2026-02-20 17:38:35 +0200

    [FIX] burger and opti

--- JS/main.js
+++ JS/main.js
@@ -31,47 +31,34 @@ for (let i = 0; i < menuLinks.length; i += 1){
 }
 }());
 //Burger end//
-
 //Scroll to anchos start//
 // Smooth scroll start
 (function () {
-
   const links = document.querySelectorAll('.js-scroll');
   const header = document.querySelector('.header');
-
   if (!links.length || !header) return;
-
   const smoothScroll = (target, duration = 800) => {
     const headerHeight = header.offsetHeight;
     const element = document.querySelector(target);
-
     if (!element) return;
-
     const targetPosition = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
     const startPosition = window.pageYOffset;
     const distance = targetPosition - startPosition;
     let startTime = null;
-
     const animation = currentTime => {
       if (!startTime) startTime = currentTime;
-
       const timeElapsed = currentTime - startTime;
       const progress = Math.min(timeElapsed / duration, 1);
-
       window.scrollTo(0, startPosition + distance * progress);
-
       if (progress < 1) requestAnimationFrame(animation);
     };
-
     requestAnimationFrame(animation);
   };
-
   links.forEach(link => {
     link.addEventListener('click', e => {
       e.preventDefault();
       smoothScroll(link.getAttribute('href'));
     });
   });
-
 })();
 // Scroll to anchos end//
